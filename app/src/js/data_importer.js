101de952c1638117bbe72ca1e3552fd7e1410926
Nick <johnsmith31338@gmail.com>
2020-05-16 12:42:38 +0300

    refactor data_importer

--- app/src/js/data_importer.js
+++ app/src/js/data_importer.js
@@ -18,15 +18,52 @@ class DataImporter {
 
     this.accumulator = [];
     this.old_ids = [];
+
+    this.type_selector = 0;
+    this.current_type = TYPES[this.type_selector];
+    this.maps = {
+      settings: {},
+      tags: {},
+      notes: {},
+      tag_notes: {},
+      note_filters: {},
+    };
   }
 
   abort() {
     this._reader.abort();
   }
 
-  add_to_accumulator(id, object) {
+  async add_to_accumulator(id, object) {
     this.accumulator.push(object);
     this.old_ids.push(id);
+
+    if(this.accumulator.length >= 100) {
+      await this.write_accumulator();
+    }
+  }
+
+  async write_accumulator() {
+    let ids = await this.notepad._storage.create_items_in_store(
+      this.current_type[0], this.accumulator
+    );
+    for(let k = 0; k < this.old_ids.length; k++) {
+      this.maps[this.current_type[0]][this.old_ids[k]] = ids[k];
+    }
+    this.old_ids = [];
+    this.accumulator = [];
+  }
+
+  map_object_links(object, type) {
+    switch(type) {
+      case "tag_note":
+        object.tag_id = this.maps.tags[object.tag_id];
+        object.note_id = this.maps.notes[object.note_id];
+        break;
+      case "note_filter":
+        object.tags = _.map(object.tags, (tag_id) => this.maps.tags[tag_id]);
+        break;
+    }
   }
 
   async execute() {
@@ -35,34 +72,19 @@ class DataImporter {
     let notepad_id = info.id;
 
     let import_error = null;
-
-    let type_selector = 0;
-    let current_type = TYPES[type_selector];
-    let maps = {
-      settings: {},
-      tags: {},
-      notes: {},
-      tag_notes: {},
-      note_filters: {},
-    };
     let info_object_recved = false;
 
-    let write_accumulator = async () => {
-      let ids = await notepad._storage.create_items_in_store(
-        current_type[0], this.accumulator
-      );
-      for(let k = 0; k < this.old_ids.length; k++) {
-        maps[current_type[0]][this.old_ids[k]] = ids[k];
-      }
-      this.old_ids = [];
-      this.accumulator = [];
-    };
+    this.notepad = notepad;
 
     let object_recved = async (object, progress) => {
       this.import_progress = Math.floor(progress * 100);
+
       let id = object.id;
       let type = object.type;
       object = _.cloneDeep(object);
+      delete object.id;
+      delete object.type;
+
       if(!info_object_recved) {
         if((type == "setting") &&
            (object.name == "info") &&
@@ -76,37 +98,29 @@ class DataImporter {
           reader.abort();
         }
       }
-      if(type == "tag_note") {
-        object.tag_id = maps.tags[object.tag_id];
-        object.note_id = maps.notes[object.note_id];
-      }
-      if(type == "note_filter") {
-        object.tags = _.map(object.tags, (tag_id) => maps.tags[tag_id]);
-      }
-      delete object.id;
-      delete object.type;
-      if(type == current_type[1]) {
-        this.add_to_accumulator(id, object);
-        if(this.accumulator.length >= 100) {
-          await write_accumulator();
-        }
+
+      this.map_object_links(object, type);
+
+      if(type == this.current_type[1]) {
+        await this.add_to_accumulator(id, object);
       } else {
-        await write_accumulator();
-        while(type_selector < TYPES.length) {
-          type_selector += 1;
-          current_type = TYPES[type_selector];
-          if(type == current_type[1]) {
-            this.add_to_accumulator(id, object);
+        await this.write_accumulator();
+        while(this.type_selector < TYPES.length) {
+          this.type_selector += 1;
+          this.current_type = TYPES[this.type_selector];
+          if(type == this.current_type[1]) {
+            await this.add_to_accumulator(id, object);
             break;
           }
         }
       }
     };
+
     let reader = new PartialFileReader(this.file, object_recved);
     this._reader = reader;
     let done = await reader.start();
     if(done) {
-      write_accumulator();
+      await this.write_accumulator();
     } else {
       import_error = "Прервано";
     }
