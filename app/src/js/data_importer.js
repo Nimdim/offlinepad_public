3ee693f2f3e772df07dfc9c48093f5488c0000cb
Nick <johnsmith31338@gmail.com>
2020-05-30 14:46:43 +0300

    remove maps

--- app/src/js/data_importer.js
+++ app/src/js/data_importer.js
@@ -17,26 +17,17 @@ class BetaDataImporterBase {
     this.import_progress = 0;
 
     this.accumulator = [];
-    this.old_ids = [];
 
     this.type_selector = 0;
     this.current_type = TYPES[this.type_selector];
-    this.maps = {
-      settings: {},
-      tags: {},
-      notes: {},
-      tag_notes: {},
-      note_filters: {},
-    };
   }
 
   abort() {
     this._reader.abort();
   }
 
-  async add_to_accumulator(id, object) {
+  async add_to_accumulator(object) {
     this.accumulator.push(object);
-    this.old_ids.push(id);
 
     if(this.accumulator.length >= 100) {
       await this.write_accumulator();
@@ -44,26 +35,10 @@ class BetaDataImporterBase {
   }
 
   async write_accumulator() {
-    let ids = await this.notepad._storage.create_items_in_store(
+    await this.notepad._storage.create_items_in_store(
       this.current_type[0], this.accumulator
     );
-    for(let k = 0; k < this.old_ids.length; k++) {
-      this.maps[this.current_type[0]][this.old_ids[k]] = ids[k];
-    }
-    this.old_ids = [];
     this.accumulator = [];
-  }
-
-  map_object_links(object, type) {
-    switch(type) {
-      case "tag_note":
-        object.tag_id = this.maps.tags[object.tag_id];
-        object.note_id = this.maps.notes[object.note_id];
-        break;
-      case "note_filter":
-        object.tags = _.map(object.tags, (tag_id) => this.maps.tags[tag_id]);
-        break;
-    }
   }
 
   async execute() {
@@ -79,10 +54,8 @@ class BetaDataImporterBase {
     let object_recved = async (object, progress) => {
       this.import_progress = Math.floor(progress * 100);
 
-      let id = object.id;
       let type = object.type;
       object = _.cloneDeep(object);
-      delete object.id;
       delete object.type;
 
       if(!info_object_recved) {
@@ -99,17 +72,15 @@ class BetaDataImporterBase {
         }
       }
 
-      this.map_object_links(object, type);
-
       if(type == this.current_type[1]) {
-        await this.add_to_accumulator(id, object);
+        await this.add_to_accumulator(object);
       } else {
         await this.write_accumulator();
         while(this.type_selector < TYPES.length) {
           this.type_selector += 1;
           this.current_type = TYPES[this.type_selector];
           if(type == this.current_type[1]) {
-            await this.add_to_accumulator(id, object);
+            await this.add_to_accumulator(object);
             break;
           }
         }
