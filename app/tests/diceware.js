235f292b7da905f89f0718564c9dcec35f3f21b8
Nick <johnsmith31338@gmail.com>
2020-06-20 03:47:10 +0300

    убраны плохие слова из ru.js

--- app/tests/diceware.js
+++ app/tests/diceware.js
@@ -6,16 +6,32 @@ import en_words from './../src/js/diceware/en.js';
 
 let DICEWARE_DICT_SIZE = 7776;
 
+let difference = function(source, for_remove) {
+    let map = {};
+    let result = []
+    _.forEach(for_remove, (item) => {map[item] = false});
+    _.forEach(source, (item) => {
+        if(map[item] == false) {
+            map[item] = true;
+        } else {
+            result.push(item);
+        }
+    });
+    return result;
+};
+
 describe("diceware tests", function() {
     it("ru unique words", async function() {
         let words = _.values(ru_words);
         let unique_words = _.uniq(words);
-        assert.equal(unique_words.length, DICEWARE_DICT_SIZE);
+        let repeats = difference(words, unique_words)
+        assert.deepEqual(repeats, []);
     });
 
     it("en unique words", async function() {
         let words = _.values(en_words);
         let unique_words = _.uniq(words);
-        assert.equal(unique_words.length, DICEWARE_DICT_SIZE);
+        let repeats = difference(words, unique_words)
+        assert.deepEqual(repeats, []);
     });
 });
